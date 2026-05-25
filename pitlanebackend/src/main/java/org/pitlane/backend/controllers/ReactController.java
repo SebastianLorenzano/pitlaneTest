90e4bbdd0d88689c92be634d0c7a1ae8e1a17af3
Sebastian Lorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2026-05-25 14:21:48 +0200

    added react controller and preparing to build react app into backend

---
+++ pitlanebackend/src/main/java/org/pitlane/backend/controllers/ReactController.java
@@ -0,0 +1,16 @@
+package org.pitlane.backend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactController {
+
+    @RequestMapping(value = {
+            "/{path:[^\\.]*}",
+            "/**/{path:[^\\.]*}"
+    })
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
